aa6b7a7540c9776e834bbf9b6719eea47ddc5574
RajanNalawade <raj.nalawade2337@gmail.com>
2026-06-26 20:48:38 +0530

    updated sonar lib plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.devtools.ksp)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.sonarqube)
 }
 
 android {
