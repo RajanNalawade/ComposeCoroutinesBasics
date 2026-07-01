913c9fdac65b67d93dc00f5281d96af7643e0492
RajanNalawade <raj.nalawade2337@gmail.com>
2026-07-01 10:53:35 +0530

    removed sonarQube static scans for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,7 @@
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("org.jetbrains.kotlin.android") version libs.versions.kotlin.get() apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.devtools.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.sonarqube)
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "RajanNalawade_ComposeCoroutinesBasics")
-        property("sonar.organization", "RajanNalawade")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
 }
